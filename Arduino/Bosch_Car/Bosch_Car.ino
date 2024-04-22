b6fcf7357ed2fe7d2ec4faf238db485e9bb25827
John Choi <johnchoi313@gmail.com>
2024-04-22 18:04:23 -0400

    All Arduino works with serial output

--- Arduino/Bosch_Car/Bosch_Car.ino
+++ Arduino/Bosch_Car/Bosch_Car.ino
@@ -5,11 +5,6 @@
 // initialize the library with the numbers of the interface pins
 LiquidCrystal_I2C lcd(0x27,16,2); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-int LCD_delay = 500;
-int LCD_timer = 0;
-
-int num = 0;
-
 //Photo Resistors (PR)
 #define HOOD_PR_PIN A1
 #define ENGINE_PR_PIN A0
@@ -19,70 +14,52 @@ int num = 0;
 #define HEADLIGHT_LS_L_PIN 12
 #define HEADLIGHT_LS_R_PIN 11
 
-
-void setup()
-{
+void setup() {
+  //Prepare I2C LCD
   lcd.init();
   lcd.backlight();
 
+  //Prepare pins
   pinMode(HOOD_PR_PIN, INPUT);
   pinMode(ENGINE_PR_PIN, INPUT);
-
   pinMode(WHEEL_PR_L_PIN, INPUT);
   pinMode(WHEEL_PR_R_PIN, INPUT);
-
   pinMode(HEADLIGHT_LS_L_PIN, INPUT_PULLUP);
   pinMode(HEADLIGHT_LS_R_PIN, INPUT_PULLUP);
-
-  Serial.begin( 9600 ); //Enable the serial comunication
-}
-
-
-void printLCD() {
   
+  //Enable the serial comunication
+  Serial.begin(115200); 
 }
 
-void loop()
-{
+//Helper Function to Print to LCD and Serial
+void printSerialLCD(String label, int value, int x, int y) {
+  lcd.setCursor(x, y);
+  lcd.print(label);
+  lcd.setCursor(x+3, y);
+  lcd.print(value);
+  Serial.print(value);
+}
 
+void loop() {
+  //Get Data from INput Pins
   int a = digitalRead(HOOD_PR_PIN);
   int b = digitalRead(ENGINE_PR_PIN);
-
   int c = digitalRead(WHEEL_PR_L_PIN);
   int d = digitalRead(WHEEL_PR_R_PIN);
+  int e = 1-digitalRead(HEADLIGHT_LS_L_PIN);
+  int f = 1-digitalRead(HEADLIGHT_LS_R_PIN);
 
-  int e = digitalRead(HEADLIGHT_LS_L_PIN);
-  int f = digitalRead(HEADLIGHT_LS_R_PIN);
+  //Display values to I2C LCD 
+  printSerialLCD("HO=",a,0,0);
+  printSerialLCD("EN=",b,0,1);
+  printSerialLCD("WL=",c,6,0);
+  printSerialLCD("WR=",d,6,1);
+  printSerialLCD("HL=",e,12,0);
+  printSerialLCD("HR=",f,12,1);
 
-  lcd.setCursor(0, 0);
-  lcd.print("HO=");
-  lcd.setCursor(0+3, 0);
-  lcd.print(a);
-  
-  lcd.setCursor(0, 1);
-  lcd.print("EN=");
-  lcd.setCursor(0+3, 1);
-  lcd.print(b);
+  //Serial Print New line
+  Serial.println();
 
-  lcd.setCursor(6, 0);
-  lcd.print("WL=");
-  lcd.setCursor(6+3, 0);
-  lcd.print(c);
-
-  lcd.setCursor(6, 1);
-  lcd.print("WR=");
-  lcd.setCursor(6+3, 1);
-  lcd.print(d);
-
-  lcd.setCursor(12, 0);
-  lcd.print("HL=");
-  lcd.setCursor(12+3, 0);
-  lcd.print(e);
- 
-  lcd.setCursor(12, 1);
-  lcd.print("HR=");
-  lcd.setCursor(12+3, 1);
-  lcd.print(f);
-  
-  delay(50);
+  //Short delay
+  delay(10);
 }
